e965800f2eee975eb6e3d4ef5b22c16805ba9365
Vladimir Alekseev <167650711+aleksvmir@users.noreply.github.com>
2025-05-31 21:18:04 +0300

    Слушатель вынесен в @BeforeEach

--- src/test/java/guru/qa/commonconfig/TestConfig.java
+++ src/test/java/guru/qa/commonconfig/TestConfig.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
@@ -15,10 +16,14 @@ public class TestConfig {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://github.com";
         Configuration.pageLoadStrategy = "eager";
+    }
+
+    @BeforeEach
+    void addListener() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
                 .screenshots(true)
                 .savePageSource(true));
-    }
+    } 
 
     @AfterAll
     static void closeBrowser() {
